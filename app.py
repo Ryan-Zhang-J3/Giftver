3c6e9988378ad5335828c378c8d3e24a28b3780c
Ryan <ryan.zhang.formal@gmail.com>
2023-12-22 13:20:37 -0500

    fixed db

--- app.py
+++ app.py
@@ -2,20 +2,13 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 app = Flask(__name__)
-app.app_context()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///participant_list.db'
 app.app_context().push()
 db = SQLAlchemy(app)
 
-
 class ParticipantList(db.Model):
+    __tablename__ = 'participant_list'
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(60), nullable = False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
@@ -28,18 +21,30 @@ class ParticipantList(db.Model):
 
 def index():
     if request.method == 'POST':
-        giftlist_content = request.form['content']
-        new_participant = ParticipantList(content=giftlist_content)
+        participant_content = request.form['content']
+        new_participant = ParticipantList(content=participant_content)
+        
         try:
             db.session.add(new_participant)
             db.session.commit()
             return redirect('/')
         except:
-            return "There was an error adding your Participant"
-
+            return 'Error adding participant'
     else:
         participants = ParticipantList.query.order_by(ParticipantList.date_created).all()
-        return render_template('index.html', participants = participants)
+        return render_template('index.html', participants=participants)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    participant_to_delete = ParticipantList.query.get_or_404(id)
+
+    try:
+        db.session.delete(participant_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error deleting participant'
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
