a03fda611629b5b8200107e8a1b6c2edcc93e35f
Ryan <ryan.zhang.formal@gmail.com>
2023-12-27 15:31:10 -0500

    email

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
 import re
+from random import shuffle
 
 app = Flask(__name__)
+#Email Functionality
+load_dotenv()
+
+mail = Mail(app)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
+
+mail = Mail(app)
+
+#Adding DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///participant_list.db'
 app.app_context().push()
 db = SQLAlchemy(app)
 
+#DB Definition
 class ParticipantList(db.Model):
     __tablename__ = 'participant_list'
     id = db.Column(db.Integer, primary_key=True)
@@ -34,10 +53,11 @@ def is_valid_email(email):
     pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
     return re.match(pattern, email)
     
-
+#Adding Participant
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
+        name = request.form['name']
         participant_content = request.form['content']
         # Check if the email is valid
         if not is_valid_email(participant_content):
@@ -48,7 +68,7 @@ def index():
         if existing_participant:
             return 'Email already exists in the database'
         
-        new_participant = ParticipantList(content=participant_content)
+        new_participant = ParticipantList(content=participant_content, name=name)
 
         # Check if 'wishlist' key is present in the form data
         participant_wishlist = request.form.get('wishlist')
@@ -66,7 +86,7 @@ def index():
         participants = ParticipantList.query.order_by(ParticipantList.date_created).all()
         return render_template('index.html', participants=participants)
     
-
+#Deleting Participant
 @app.route('/delete/<int:id>')
 def delete(id):
     participant_to_delete = ParticipantList.query.get_or_404(id)
@@ -77,12 +97,15 @@ def delete(id):
         return redirect('/')
     except:
         return 'Error deleting participant'
-    
+
+
+#Updating Participant
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     participant_to_update = ParticipantList.query.get_or_404(id)
     if request.method == 'POST':
         participant_to_update.content = request.form['content']
+        participant_to_update.name = request.form['name']
         
         wishlist_value = request.form.get('wishlist')
         if wishlist_value:
@@ -100,7 +123,43 @@ def update(id):
     else:
         wishlist_value = participant_to_update.wishlist.wishlist_content if participant_to_update.wishlist else None
         return render_template('update.html', participant=participant_to_update, wishlist=wishlist_value)  
+
+
+@app.route('/start_gift_exchange')
+def start_gift_exchange():
+    participants = ParticipantList.query.all()
+
+    # Get the participants' wishlists
+    participants_wishlists = {participant.id: participant.wishlist.wishlist_content if participant.wishlist else None for participant in participants}
+
+    # Shuffle the participants
+    shuffled_participants = participants.copy()
+    shuffle(shuffled_participants)
+
+    # Assign secret Santa recipients
+    for i in range(len(participants)):
+        participant = shuffled_participants[i]
+        recipient = shuffled_participants[(i + 1) % len(participants)]
+        participant.recipient_id = recipient.id
+
+    # Send emails to participants
+    with mail.connect() as conn:
+        for participant in participants:
+            recipient = ParticipantList.query.get(participant.recipient_id)
+            recipient_name = recipient.name
+            recipient_wishlist = participants_wishlists[recipient.id]
+
+            subject = 'Secret Santa Gift Exchange'
+            body = f'Hello {participant.name}!\n\nYou have been assigned {recipient_name} as your secret Santa recipient.\nTheir wishlist: {recipient_wishlist}\n\nHappy gifting!'
+            msg = Message(subject=subject, body=body, sender=app.config['MAIL_USERNAME'], recipients=[participant.content])
+            conn.send(msg)
+
+    try:
+        db.session.commit()
+        return 'Secret Santa gift exchange initiated'
+    except:
+        return 'Error starting gift exchange'
     
-# Existing code...  
+
 if __name__ == "__main__":
     app.run(debug=True)
